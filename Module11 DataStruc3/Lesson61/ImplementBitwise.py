17340cd00208d95783d9238612408e68f83cc44b
ManjuCodingal <manju17.codingal@gmail.com>
2025-10-01 15:18:47 +0530

    changes

---
+++ Module11 DataStruc3/Lesson61/ImplementBitwise.py
@@ -0,0 +1,29 @@
+num1 = 10 
+num2 = 4
+print("num1 & num2 are:", num1, num2)
+print("num1 | num2 are:", num1 | num2)
+print("num1 ^ num2 are:", num1 ^ num2)
+print("num1 << num2 are:", num1 << num2)
+print("num1 >> num2 are:", num1 >> num2)
+print("~num1 are:", ~num1)
+print("~num2 are:", ~num2)
+
+# & (and)
+
+# 0 0 = 0
+# 1 1 = 1
+# 0 1 = 0
+# 1 0 = 0
+
+# | (or)
+
+# 0 0 = 0
+# 1 1 = 1
+# 0 1 = 1
+# 1 0 = 1
+
+# ^ x0r
+# 0 0 = 0
+# 1 1 = 0
+# 0 1 = 1
+# 1 0 = 1
